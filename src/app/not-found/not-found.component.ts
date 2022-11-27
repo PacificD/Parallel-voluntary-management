0d1bcb47c0fad0acbf96864294cbb9add777aadc
PacificD <1484636442@qq.com>
2022-11-27 22:49:31 +0800

    feat(not-found): page

---
+++ src/app/not-found/not-found.component.ts
@@ -0,0 +1,16 @@
+/*
+ * @Author: Pacific_D
+ * @Date: 2022-11-26 15:59:05
+ * @LastEditTime: 2022-11-26 20:41:23
+ * @LastEditors: Pacific_D
+ * @Description:
+ * @FilePath: \Parallel-voluntary-management\src\app\not-found\not-found.component.ts
+ */
+import { Component } from "@angular/core"
+
+@Component({
+  selector: "app-not-found",
+  templateUrl: "./not-found.component.html",
+  styleUrls: ["./not-found.component.css"]
+})
+export class NotFoundComponent {}
