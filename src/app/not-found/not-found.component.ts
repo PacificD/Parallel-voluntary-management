b3566344bed9cee57a83d6c384b30c718a0f0bdd
PacificD <1484636442@qq.com>
2022-11-28 22:45:00 +0800

    merge(not-found): page

--- src/app/not-found/not-found.component.ts
+++ src/app/not-found/not-found.component.ts
@@ -1,19 +1,18 @@
 /*
  * @Author: Pacific_D
  * @Date: 2022-11-26 15:59:05
- * @LastEditTime: 2022-11-26 20:41:23
+ * @LastEditTime: 2022-11-28 22:44:42
  * @LastEditors: Pacific_D
  * @Description:
  * @FilePath: \Parallel-voluntary-management\src\app\not-found\not-found.component.ts
  */
 import { Component } from "@angular/core"
 import fadeIn from "../common/animations/fadeIn"
-import { aniOne,aniTwo,aniThree,aniFour,aniFive,aniSix } from "./not-find.animation"
+import { aniOne, aniTwo, aniThree, aniFour, aniFive, aniSix } from "./not-find.animation"
 @Component({
   selector: "app-not-found",
   templateUrl: "./not-found.component.html",
   styleUrls: ["./not-found.component.css"],
-  animations: [fadeIn(), aniOne,aniTwo,aniThree,aniFour,aniFive,aniSix]
-
+  animations: [fadeIn(), aniOne, aniTwo, aniThree, aniFour, aniFive, aniSix]
 })
 export class NotFoundComponent {}
