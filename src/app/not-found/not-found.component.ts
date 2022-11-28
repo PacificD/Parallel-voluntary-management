dd24f05c5fd4ab1832b63aaa938a074fc8df9399
3120005395谭圣霖 <1737117947@qq.com>
2022-11-28 16:32:08 +0800

    404page

--- src/app/not-found/not-found.component.ts
+++ src/app/not-found/not-found.component.ts
@@ -7,10 +7,13 @@
  * @FilePath: \Parallel-voluntary-management\src\app\not-found\not-found.component.ts
  */
 import { Component } from "@angular/core"
-
+import fadeIn from "../common/animations/fadeIn"
+import { aniOne,aniTwo,aniThree,aniFour,aniFive,aniSix } from "./not-find.animation"
 @Component({
   selector: "app-not-found",
   templateUrl: "./not-found.component.html",
-  styleUrls: ["./not-found.component.css"]
+  styleUrls: ["./not-found.component.css"],
+  animations: [fadeIn(), aniOne,aniTwo,aniThree,aniFour,aniFive,aniSix]
+
 })
 export class NotFoundComponent {}
