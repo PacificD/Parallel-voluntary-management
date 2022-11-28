7c80526434714b600f351a14b853609231c24159
PacificD <1484636442@qq.com>
2022-11-28 22:39:24 +0800

    feat(components): button

--- src/app/common/components/button/button.component.ts
+++ src/app/common/components/button/button.component.ts
@@ -1,16 +1,18 @@
 /*
  * @Author: Pacific_D
  * @Date: 2022-11-27 18:09:26
- * @LastEditTime: 2022-11-27 18:14:03
+ * @LastEditTime: 2022-11-28 16:45:01
  * @LastEditors: Pacific_D
  * @Description:
  * @FilePath: \Parallel-voluntary-management\src\app\common\components\button\button.component.ts
  */
-import { Component } from "@angular/core"
+import { Component, Input } from "@angular/core"
 
 @Component({
   selector: "app-button",
   templateUrl: "./button.component.html",
   styleUrls: ["./button.component.css"]
 })
-export class ButtonComponent {}
+export class ButtonComponent {
+  @Input() content = "Button"
+}
