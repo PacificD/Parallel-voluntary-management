abb6dfb24c3d5c8a404efc974d253f9c1ab6ac6a
PacificD <1484636442@qq.com>
2022-12-03 15:42:35 +0800

    env file

---
+++ src/app/secrets.ts
@@ -0,0 +1,13 @@
+/*
+ * @Author: Pacific_D
+ * @Date: 2022-11-29 17:23:18
+ * @LastEditTime: 2022-12-03 10:26:14
+ * @LastEditors: Pacific_D
+ * @Description:
+ * @FilePath: \Parallel-voluntary-management\src\app\secrets.ts
+ */
+const BASE_URL = "http://43.138.234.145:9090",
+  KEY = "ef14c08d54c21a98",
+  IV = "ef94d14b51b20c73"
+
+export { BASE_URL, KEY, IV }
